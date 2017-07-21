a67d9be2c69402079e5bab33148078640806f9c6
wwwwwwwwwwwwwwwwwwwwwwwwwwwwww <xxperipsis@gmail.com>
2017-07-21 06:25:17 -0500

    Only emit locals if function contains them

--- src/BlockConverter.cc
+++ src/BlockConverter.cc
@@ -19,31 +19,37 @@ string wdis::Convert::getFuncBody(Module* mod, Function* fn, bool addExtraInfo) 
 	string fnBody;
 	fnBody += " {\n";
 	// Convert function locals to intermediate locals
-	vector<InterLocal> locals;
 	vector<WasmType>* vars = &(fn->vars);
-	for (int i = 0; i < vars->size(); ++i) {
-		// Fill locals vector
-		InterLocal il(fn, i);
-		locals.push_back(il);
-	}
-	fnBody += "\t// Parsed WASM function locals:\n";
-	// Convert intermediates to C declorations
-	for (auto& ilocal : locals) {
-		string cdecl = ilocal.getCDecloration();
-		fnBody += "\t";
-		fnBody += cdecl;
-		if (addExtraInfo) {
-			// Initialize locals to 0 with extra info enabled
-			fnBody += " = 0";
+	if (vars->size()) {
+		vector<InterLocal> locals;
+		for (int i = 0; i < vars->size(); ++i) {
+			// Fill locals vector
+			InterLocal il(fn, i);
+			locals.push_back(il);
 		}
-		fnBody += "; ";
-		if (addExtraInfo) {
-			// Local info
-			fnBody += "// Local with index '" + to_string(ilocal.index) + "'";
+		fnBody += "\t// Parsed WASM function locals:\n";
+		// Convert intermediates to C declorations
+		for (auto& ilocal : locals) {
+			string cdecl = ilocal.getCDecloration();
+			fnBody += "\t";
+			fnBody += cdecl;
+			if (addExtraInfo) {
+				// Initialize locals to 0 with extra info enabled
+				fnBody += " = 0";
+			}
+			fnBody += "; ";
+			if (addExtraInfo) {
+				// Local info
+				fnBody += "// Local with index '" + to_string(ilocal.index) + "'";
+			}
+			fnBody += "\n";
 		}
-		fnBody += "\n";
+		if (addExtraInfo) fnBody += "\n";
+	} else {
+		if (addExtraInfo) {
+			fnBody += "// Function contains no non-parameter locals\n";
+		}
 	}
-	if (addExtraInfo) fnBody += "\n";
 	// Function bodies are block expressions
 	fnBody += Convert::parseExpr(mod, fn->body, -1);
 	fnBody += "}";
