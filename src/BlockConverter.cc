376a51362af19ea4b618cf7605741f27b0bf3ef7
wwwwwwwwwwwwwwwwwwwwwwwwwwwwww <xxperipsis@gmail.com>
2017-07-21 05:05:00 -0500

    Namespace BlockConverter method calls

--- src/BlockConverter.cc
+++ src/BlockConverter.cc
@@ -9,7 +9,7 @@ string wdis::Convert::getBlockBody(Module* mod, Block* blck, int depth) {
 	stringstream s;
 	depth++;
 	for (auto& expr : blck->list) {
-		s << parseExpr(mod, expr, depth);
+		s << Convert::parseExpr(mod, expr, depth);
 	}
 	depth--;
 	return s.str();
@@ -18,7 +18,7 @@ string wdis::Convert::getFuncBody(Module* mod, Function* fn, bool addExtraInfo) 
 	string fnBody;
 	fnBody += " {\n";
 	// Function bodies are block expressions
-	fnBody += parseExpr(mod, fn->body, -1);
+	fnBody += Convert::parseExpr(mod, fn->body, -1);
 	fnBody += "}";
 	return fnBody;
 }
