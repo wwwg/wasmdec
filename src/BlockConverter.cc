a12c18ee0dbaef96fd0bc6eb0b434823b554c439
wwwwwwwwwwwwwwwwwwwwwwwwwwwwww <xxperipsis@gmail.com>
2017-07-21 06:00:23 -0500

    Add IntermediateLocal class

--- src/BlockConverter.cc
+++ src/BlockConverter.cc
@@ -1,6 +1,7 @@
 #ifndef _CONVER_H
 #include "Conversion.h"
 #endif
+#include "IntermediateLocal.h"
 using namespace std;
 using namespace wasm;
 
@@ -17,6 +18,11 @@ string wdis::Convert::getBlockBody(Module* mod, Block* blck, int depth) {
 string wdis::Convert::getFuncBody(Module* mod, Function* fn, bool addExtraInfo) {
 	string fnBody;
 	fnBody += " {\n";
+	if (addExtraInfo) {
+		fnBody += "\t// Function locals:\n";
+	}
+	// TODO : Parse local variables
+	if (addExtraInfo) fnBody += "\n";
 	// Function bodies are block expressions
 	fnBody += Convert::parseExpr(mod, fn->body, -1);
 	fnBody += "}";
---
+++ src/IntermediateLocal.h
@@ -0,0 +1,32 @@
+#ifndef _INTER_LOCAL_H
+#define _INTER_LOCAL_H
+
+// Hack to prevent compiler issues
+#ifndef _CONVER_H
+#include "Conversion.h"
+#else
+extern class Convert;
+#endif
+
+namespace wdis {
+	class InterLocal {
+	public:
+		wasm::Function* fn;
+		int index;
+		string CLocal;
+		wasm::WasmType wasmLocal;
+		InterLocal(wasm::Function* _fn, int _index) {
+			fn = _fn;
+			index = _fn->params.size() + _index;
+			wasmLocal = _fn->vars[index];
+			CLocal = Convert::resolveType(wasmLocal);
+		}
+		string getCDecloration(void) {
+			string ret = CLocal;
+			ret += " " + Convert::getLocal(index);
+			return ret
+		}
+	};
+} // namespace wdis
+
+#endif // _INTER_LOCAL_H
