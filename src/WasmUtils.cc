54a3bba729c81a38b4f170aebb4e729aee2c0437
wwwwwwwwwwwwwwwwwwwwwwwwwwwwww <xxperipsis@gmail.com>
2017-07-21 07:25:12 -0500

    Seperate utils to source file

---
+++ src/WasmUtils.cc
@@ -0,0 +1,51 @@
+#include "WasmUtils.h"
+using namespace wdis;
+
+FunctionType* util::resolveFType(Module* m, Name nm) {
+	for (int i = 0; i < m->functionTypes.size(); ++i) {
+		if (m->functionTypes[i]->name == nm) {
+			return m->functionTypes[i].get();
+		}
+	}
+	return nullptr;
+}
+string util::tab(int tabTimes) {
+	// Util for generating nicer looking C
+	string ret;
+	for (int i = 0; i < tabTimes; ++i) {
+		ret += "\t";
+	}
+	return ret;
+}
+string util::getLiteralValue(Literal* val) {
+	int32_t conv_i32;
+	int64_t conv_i64;
+	float conv_f32;
+	double conv_f64;
+	switch (val->type) {
+		// Convert constant literal type to string
+		case WasmType::none:
+		case WasmType::unreachable:
+			return "0";
+			break;
+		case WasmType::i32:
+			conv_i32 = val->geti32();
+			return to_string(conv_i32);
+			break;
+		case WasmType::i64:
+			conv_i64 = val->geti64();
+			return to_string(conv_i64);
+			break;
+		case WasmType::f32:
+			conv_f32 = val->getf32();
+			return to_string(conv_f32);
+			break;
+		case WasmType::f64:
+			conv_f64 = val->getf64();
+			return to_string(conv_f64);
+			break;
+	}
+}
+int util::getLocalIndex(Function* fn, int index) {
+	return (&fn->params)->size() + index;
+}
--- src/WasmUtils.h
+++ src/WasmUtils.h
@@ -1,5 +1,6 @@
 #ifndef _WASM_UTILS
 #define _WASM_UTILS
+#include "convert/Conversion.h"
 
 using namespace wasm;
 using namespace std;
@@ -7,54 +8,10 @@ using namespace std;
 namespace wdis {
 	class util {
 	public:
-		static FunctionType* resolveFType(Module* m, Name nm) {
-			for (int i = 0; i < m->functionTypes.size(); ++i) {
-				if (m->functionTypes[i]->name == nm) {
-					return m->functionTypes[i].get();
-				}
-			}
-			return nullptr;
-		}
-		static string tab(int tabTimes) {
-			// Util for generating nicer looking C
-			string ret;
-			for (int i = 0; i < tabTimes; ++i) {
-				ret += "\t";
-			}
-			return ret;
-		}
-		static string getLiteralValue(Literal* val) {
-			int32_t conv_i32;
-			int64_t conv_i64;
-			float conv_f32;
-			double conv_f64;
-			switch (val->type) {
-				// Convert constant literal type to string
-				case WasmType::none:
-				case WasmType::unreachable:
-					return "0";
-					break;
-				case WasmType::i32:
-					conv_i32 = val->geti32();
-					return to_string(conv_i32);
-					break;
-				case WasmType::i64:
-					conv_i64 = val->geti64();
-					return to_string(conv_i64);
-					break;
-				case WasmType::f32:
-					conv_f32 = val->getf32();
-					return to_string(conv_f32);
-					break;
-				case WasmType::f64:
-					conv_f64 = val->getf64();
-					return to_string(conv_f64);
-					break;
-			}
-		}
-		static int getLocalIndex(Function* fn, int index) {
-			return (&fn->params)->size() + index;
-		}
+		static FunctionType* resolveFType(Module*, Name);
+		static string tab(int);
+		static string getLiteralValue(Literal*);
+		static int getLocalIndex(Function*, int);
 	};
 }; // namespace wdis
 
