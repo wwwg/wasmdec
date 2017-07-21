5949cc80424d93ba2e7f3f2a778e42ff3709c216
wwwwwwwwwwwwwwwwwwwwwwwwwwwwww <xxperipsis@gmail.com>
2017-07-21 07:30:13 -0500

    Ignore object files

---
+++ src/CodeGen.cc
@@ -0,0 +1,71 @@
+#include "CodeGen.h"
+using namespace wdis;
+using namespace std;
+
+CodeGenerator::CodeGenerator(vector<char>* inbin, bool useDebug, bool _emitExtraData) : binary((*inbin)), parser(module, binary, useDebug) {
+	isDebug = useDebug;
+	emitExtraData = _emitExtraData;
+	debug("Parsing wasm binary...\n");
+	// Attempt to parse binary via Binaryen's AST parser
+	try {
+		parser.read();
+	} catch (wasm::ParseException& err) {
+		err.dump(cerr);
+		cout << "Failed to parse wasm binary." << endl;
+		return;
+	}
+	debug("Parsed bin successfully.\n");
+}
+void CodeGenerator::gen() {
+	debug("Starting code generation...\n");
+	// Process imports
+	if (module.imports.size()) {
+		emit.comment("WASM imports:");
+		for (auto& i : module.imports) {
+			// cout << "Got import: " << i->name << endl;
+			string decl = Convert::getDecl(&module, i);
+			emit << decl << endl;
+		}
+	} else {
+		// No imports, so just leave a comment
+		emit.comment("No WASM imports.");
+		emit.ln();
+	}
+	// Process functions
+	if (parser.functions.size()) {
+		emit.comment("WASM functions:");
+		emit.ln();
+		for (const auto &func : parser.functions) {
+			if (emitExtraData) {
+				// Emit information about the function as a comment
+				wasm::Block* fnBody = func->body->cast<wasm::Block>();
+				emit << "/*" << endl
+				<< "\tFunction '" << func->name << "'" << endl
+				<< "\tLocal variables: " << func->vars.size() << endl
+				<< "\tParameters: " << func->params.size() << endl
+				<< "\tImmediate block expressions: " << fnBody->list.size() << endl
+				<< "*/" << endl;
+			}
+			Context ctx(func, &module);
+			emit << Convert::getDecl(func) << Convert::getFuncBody(&ctx, emitExtraData) << endl;
+		}
+	} else {
+		emit.comment("No WASM functions.");
+		emit.ln();
+	}
+	debug("Code generation complete.\n");
+}
+string CodeGenerator::getEmittedCode() {
+	return emit.getCode();
+}
+// Debug functions
+void CodeGenerator::debug(string msg) {
+	if (isDebug) {
+		cerr << "wdis CodeGen: " << msg;
+	}
+}
+void CodeGenerator::debugf(string msg) {
+	if (isDebug) {
+		cerr << msg;
+	}
+}
--- src/CodeGen.h
+++ src/CodeGen.h
@@ -15,73 +15,12 @@ namespace wdis {
 		bool isDebug;
 		bool emitExtraData;
 		Emitter emit;
-		CodeGenerator(vector<char>* inbin, bool useDebug, bool _emitExtraData) : binary((*inbin)), parser(module, binary, useDebug) {
-			isDebug = useDebug;
-			emitExtraData = _emitExtraData;
-			debug("Parsing wasm binary...\n");
-			// Attempt to parse binary via Binaryen's AST parser
-			try {
-				parser.read();
-			} catch (wasm::ParseException& err) {
-				err.dump(cerr);
-				cout << "Failed to parse wasm binary." << endl;
-				return;
-			}
-			debug("Parsed bin successfully.\n");
-		}
-		void gen() {
-			debug("Starting code generation...\n");
-			// Process imports
-			if (module.imports.size()) {
-				emit.comment("WASM imports:");
-				for (auto& i : module.imports) {
-					// cout << "Got import: " << i->name << endl;
-					string decl = Convert::getDecl(&module, i);
-					emit << decl << endl;
-				}
-			} else {
-				// No imports, so just leave a comment
-				emit.comment("No WASM imports.");
-				emit.ln();
-			}
-			// Process functions
-			if (parser.functions.size()) {
-				emit.comment("WASM functions:");
-				emit.ln();
-				for (const auto &func : parser.functions) {
-					if (emitExtraData) {
-						// Emit information about the function as a comment
-						wasm::Block* fnBody = func->body->cast<wasm::Block>();
-						emit << "/*" << endl
-						<< "\tFunction '" << func->name << "'" << endl
-						<< "\tLocal variables: " << func->vars.size() << endl
-						<< "\tParameters: " << func->params.size() << endl
-						<< "\tImmediate block expressions: " << fnBody->list.size() << endl
-						<< "*/" << endl;
-					}
-					Context ctx(func, &module);
-					emit << Convert::getDecl(func) << Convert::getFuncBody(&ctx, emitExtraData) << endl;
-				}
-			} else {
-				emit.comment("No WASM functions.");
-				emit.ln();
-			}
-			debug("Code generation complete.\n");
-		}
-		string getEmittedCode() {
-			return emit.getCode();
-		}
+		CodeGenerator(vector<char>*, bool, bool);
+		void gen();
+		string getEmittedCode();
 	protected:
-		void debug(string msg) {
-			if (isDebug) {
-				cerr << "wdis CodeGen: " << msg;
-			}
-		}
-		void debugf(string msg) {
-			if (isDebug) {
-				cerr << msg;
-			}
-		}
+		void debug(string);
+		void debugf(string);
 	};
 } // namespace wdis
 
