5b1d7b4ea086ae714e9b84761e80664c90dd1f16
wwwwwwwwwwwwwwwwwwwwwwwwwwwwww <xxperipsis@gmail.com>
2018-08-08 19:07:48 -0500

    use default command in test script

--- test/tests.sh
+++ test/tests.sh
@@ -2,10 +2,9 @@
 
 # hacky test script for wasmdec
 # make sure you have wasmdec installed before running (run "make install" in the root dir)
-WASMDEC=wasmdec
 
 do_test () {
-	WASMDEC -i $1 -o test.c -e -d
+	wasmdec -i $1 -o test.c -e -d
 	if [ $? -eq 0 ]; then
 		# test succeeded!
 		echo "TEST SUCCESS: test $1 passed"
