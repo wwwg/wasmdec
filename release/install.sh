d6afed89210eb83e63038bebfb93aeedfd7ce902
ultratypebot <xxperipsis@gmail.com>
2018-08-28 17:49:39 -0500

    finish release install script

--- release/install.sh
+++ release/install.sh
@@ -10,6 +10,10 @@ fi
 if [ -e wasmdec ]
 then
     echo "installing wasmdec"
+    if [ -d "/usr/lib64" ]; then cp libbinaryen.so /usr/lib64/; else cp libbinaryen.so /usr/lib/; fi
+	cp ./wasmdec /usr/bin
+	echo "wasmdec installed successfully."
+	exit 0
 else
     echo "no wasmdec binary exists in this directory, something is horribly wrong."
     exit 1
