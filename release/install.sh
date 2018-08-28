284668f05b07ef8555d7f280086126d9b5c57941
ultratypebot <xxperipsis@gmail.com>
2018-08-28 17:46:55 -0500

    add binary check in install.sh

--- release/install.sh
+++ release/install.sh
@@ -6,3 +6,11 @@ if [[ $EUID -ne 0 ]]; then
    echo "you must be root to install wasmdec" 
    exit 1
 fi
+
+if [ -e wasmdec ]
+then
+    echo "installing wasmdec"
+else
+    echo "no wasmdec binary exists in this directory, something is horribly wrong."
+    exit 1
+fi
