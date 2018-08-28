7fdcd44b98c8cbf039dadaad6fc296f3c969791e
ultratypebot <xxperipsis@gmail.com>
2018-08-28 17:41:01 -0500

    create mkrelease script

---
+++ mkrelease.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# simple script to export files to the release/ directory
+
+if [[ $EUID -ne 0 ]]; then
+   echo "you must be root" 
+   exit 1
+fi
+
+mkdir -p release/
+
+make binaryen
+make default
+
+cp wasmdec release/
+cp external/binaryen/lib/libbinaryen.so release/
