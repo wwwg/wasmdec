02dfc62b15ce4d648189894f70142b5f3a5ea4ef
ultratypebot <xxperipsis@gmail.com>
2018-08-29 19:43:31 -0500

    add fnPreface string to disasm config

--- src/decompiler/DisasmConfig.h
+++ src/decompiler/DisasmConfig.h
@@ -13,12 +13,14 @@ public:
     bool debug;
     bool extra;
     bool includePreamble;
+    string fnPreface;
     DisasmMode mode;
     inline DisasmConfig(bool _debug, bool _extra, DisasmMode _mode) {
         debug = _debug;
         extra = extra;
         mode = _mode;
         includePreamble = true;
+        fnPreface = "";
     }
 };
 
