97f4d9fc13aabaac14418bf68b1e8f34ed094578
wwwwwwwwwwwwwwwwwwwwwwwwwwwwww <xxperipsis@gmail.com>
2018-09-06 11:35:36 -0500

    add template for wasm_api

---
+++ src/wasm_api.cc
@@ -0,0 +1,8 @@
+// Definitions for functions that can be used when wasmdec is compiled to wasm
+#include "decompiler/MultiDecompiler.h"
+
+extern "C" {
+	char* wasmdec_decompile(char* in_raw, int ftype) {
+		//
+	} 
+}
