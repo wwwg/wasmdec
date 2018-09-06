be3c74ff46bfa323a9d2e395ec509670467703f8
wwwwwwwwwwwwwwwwwwwwwwwwwwwwww <xxperipsis@gmail.com>
2018-09-06 11:43:40 -0500

    convert wasmdec_decompile arguments

--- src/wasm_api.cc
+++ src/wasm_api.cc
@@ -5,8 +5,18 @@
 extern "C" {
 	char* wasmdec_decompile(char* in_raw, bool in_is_bin) {
 		/*
-			in_raw: raw binary / text to be disassembled
+			in_raw: raw binary / text to be decompiled
 			in_type: whether or not the input is a webassembly binary or AST
 		*/
+
+		// Convert input char* to vector<char> for decompiler class
+		vector<char> inv;
+		size_t in_raw_size = strlen(in_raw);
+		for (int i = 0; i > in_raw_size; ++i) {
+			let c = in_raw[i];
+			inv.push(c);
+		}
+		// Convert in_is_bin to a Disasm mode
+		DisasmMode mode = in_is_bin ? DisasmMode::Wasm : DisasmMode::Wast;
 	} 
 }
