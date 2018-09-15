ee8300b046cf2e56af9568020e6b2b1ff977880e
wwwg <joshpauten@gmail.com>
2018-09-14 23:57:46 -0500

    init wasmdec.js after wasm functions are detected

--- wasmdec.js/wasmdec.js
+++ wasmdec.js/wasmdec.js
@@ -1,9 +1,20 @@
 (() => {
-	window.Wasmdec = {};
+	window.Wasmdec = {
+		ready: false
+	};
 	let glue = document.createElement('script');
 	glue.src = 'https://rawgit.com/wwwg/wasmdec/master/wasmdec.js/wasmdec.wasm.js';
 	document.head.appendChild(glue);
 	glue.onload = () => {
-		//
+		let intv = setInterval(() => {
+			if (Module["_wasmdec_create_decompiler"]) {
+				init();
+				clearInterval(intv);
+				return;
+			}
+		});
+	}
+	let init = () => {
+		Wasmdec.ready = true;
 	}
 })();
