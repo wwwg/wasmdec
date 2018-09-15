1479cece44cd858b17548ad06035d811fb48352e
wwwg <joshpauten@gmail.com>
2018-09-14 22:33:42 -0500

    remove test api function

--- src/wasm_api.cc
+++ src/wasm_api.cc
@@ -40,39 +40,4 @@ extern "C" {
 		Decompiler* ret = new Decompiler(conf, inv);
 		return ret;
 	}
-	char* wasmdec_decompile(char* in_raw, bool in_is_bin) {
-		/*
-			in_raw: raw binary / text to be decompiled
-			in_type: whether or not the input is a webassembly binary or AST
-		*/
-
-		// Convert input char* to vector<char> for decompiler class
-		vector<char> inv;
-		size_t in_raw_size = strlen(in_raw);
-		for (unsigned int i = 0; i < in_raw_size; ++i) {
-			char c = in_raw[i];
-			inv.push_back(c);
-		}
-		// Convert in_is_bin to a Disasm mode
-		DisasmMode mode = in_is_bin ? DisasmMode::Wasm : DisasmMode::Wast;
-		DisasmConfig conf(true, true, mode);
-		// create the decompiler
-		Decompiler d(conf, inv);
-		d.decompile();
-		// Get decompiler result and turn it into a valid return value
-		if (d.failed()) {
-			printf("WARN: wasmdec decomp failed\n");
-			return nullptr;
-		} else {
-			string out = d.getEmittedCode();
-			size_t ret_s = out.length() + 1;
-			char* ret = (char*)malloc(ret_s);
-			memset(ret, 0x0, ret_s);
-			for (unsigned int i = 0; i < out.size(); ++i) {
-				char c = out.at(i);
-				ret[i] = c;
-			}
-			return ret;
-		}
-	} 
 }
