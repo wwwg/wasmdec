d3a1761c829898d91d2bc809d8725113c2b47250
wwwg <joshpauten@gmail.com>
2018-09-14 23:45:05 -0500

    append glue script in api script

--- wasmdec.js/wasmdec.js
+++ wasmdec.js/wasmdec.js
@@ -1,3 +1,9 @@
 (() => {
 	window.Wasmdec = {};
+	let glue = document.createElement('script');
+	glue.src = 'https://rawgit.com/wwwg/wasmdec/master/wasmdec.js/wasmdec.wasm.js';
+	document.head.appendChild(glue);
+	glue.onload = () => {
+		//
+	}
 })();
