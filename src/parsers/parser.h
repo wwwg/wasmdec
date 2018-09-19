00cc66b31530fea20526707b286dee1259a1855d
wwwg <joshpauten@gmail.com>
2018-09-19 09:50:41 -0500

    define parsers for each wasm expression

--- src/parsers/parser.h
+++ src/parsers/parser.h
@@ -7,7 +7,27 @@ namespace wasmdec {
     typedef string (*WasmParser) (Context*, Expression*);
     namespace parsers {
         WasmParser block;
-    };
-};
-
-#endif
+        WasmParser binary;
+        WasmParser get_local;
+        WasmParser _return;
+        WasmParser _if;
+        WasmParser _const;
+        WasmParser nop;
+        WasmParser get_global;
+        WasmParser set_global;
+        WasmParser _break;
+        WasmParser call;
+        WasmParser call_import;
+        WasmParser loop;
+        WasmParser _switch;
+        WasmParser call_indirect;
+        WasmParser set_local;
+        WasmParser load;
+        WasmParser store;
+        WasmParser unary;
+        WasmParser select;
+        WasmParser drop;
+        WasmParser host;
+        WasmParser unreachable;
+    }
+}
