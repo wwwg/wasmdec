cfaf84eddb53316d69117dcb6b1ebd60497109a0
wwwg <joshpauten@gmail.com>
2018-09-19 09:57:27 -0500

    define all parser functions without a special type

--- src/parsers/parser.h
+++ src/parsers/parser.h
@@ -6,28 +6,28 @@
 namespace wasmdec {
     namespace parsers {
         string block(Context*, Expression*);
-        WasmParser binary;
-        WasmParser get_local;
-        WasmParser _return;
-        WasmParser _if;
-        WasmParser _const;
-        WasmParser nop;
-        WasmParser get_global;
-        WasmParser set_global;
-        WasmParser _break;
-        WasmParser call;
-        WasmParser call_import;
-        WasmParser loop;
-        WasmParser _switch;
-        WasmParser call_indirect;
-        WasmParser set_local;
-        WasmParser load;
-        WasmParser store;
-        WasmParser unary;
-        WasmParser select;
-        WasmParser drop;
-        WasmParser host;
-        WasmParser unreachable;
+        string binary(Context*, Expression*);
+        string get_local(Context*, Expression*);
+        string _return(Context*, Expression*);
+        string _if(Context*, Expression*);
+        string _const(Context*, Expression*);
+        string nop(Context*, Expression*);
+        string get_global(Context*, Expression*);
+        string set_global(Context*, Expression*);
+        string _break(Context*, Expression*);
+        string call(Context*, Expression*);
+        string call_import(Context*, Expression*);
+        string loop(Context*, Expression*);
+        string _switch(Context*, Expression*);
+        string call_indirect(Context*, Expression*);
+        string set_local(Context*, Expression*);
+        string load(Context*, Expression*);
+        string store(Context*, Expression*);
+        string unary(Context*, Expression*);
+        string select(Context*, Expression*);
+        string drop(Context*, Expression*);
+        string host(Context*, Expression*);
+        string unreachable(Context*, Expression*);
     }
 }
 
