4c3921c4f4a170cc90440f748fbe50d129291f5f
wwwg <joshpauten@gmail.com>
2018-09-23 21:32:16 -0500

    add load parser

---
+++ src/parsers/load.cc
@@ -0,0 +1,14 @@
+#include "parser.h"
+using namespace wasmdec;
+
+string wasmdec::parsers::load(Context* ctx, Expression* ex) {
+	string ret;
+	// Memory loading
+    Load* lxp = ex->cast<Load>();
+    ctx->lastExpr = ex;
+    ctx->functionLevelExpression = false;
+    string var = Convert::parseExpr(ctx, lxp->ptr);
+    ret += util::tab(ctx->depth);
+    ret += var;
+	return ret;
+}
