9b1c1d28af0fbcd20134e0d8a7d7e53849fc8b5d
wwwg <joshpauten@gmail.com>
2018-09-23 22:00:09 -0500

    fix typo in atomics source file

--- src/parsers/atomics.cc
+++ src/parsers/atomics.cc
@@ -1,6 +1,6 @@
 #include "parser.h"
 using namespace wasmdec;
 
-string wasmdec::parsers::atmoics(Context* ctx, Expression* ex) {
+string wasmdec::parsers::atomics(Context* ctx, Expression* ex) {
 	return "/* Atomic operation unsupported */\n";
 }
