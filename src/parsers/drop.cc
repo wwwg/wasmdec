b861c2b1490a972f5ff3b22af7b2554f74c63902
wwwg <joshpauten@gmail.com>
2018-09-23 21:37:24 -0500

    implement drop parser

--- src/parsers/drop.cc
+++ src/parsers/drop.cc
@@ -4,7 +4,14 @@ using namespace wasmdec;
 string wasmdec::parsers::drop(Context* ctx, Expression* ex) {
 	string ret;
 	
-    // 
+    Drop* dex = ex->cast<Drop>();
+    ret += util::tab(1);
+    ret += "/* Drop routine */\n";
+    ctx->functionLevelExpression = false;
+    ctx->lastExpr = ex;
+    ret += parseExpr(ctx, dex->value);
+    ret += util::tab(1);
+    ret += "/* End of drop routine */\n";
 
 	return ret;
 }
